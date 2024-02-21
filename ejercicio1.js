db6f0e6822d34f76de02c6a6a4f418139ff569ca
JuanSebastian2107 <158063963+JuanSebastian2107@users.noreply.github.com>
2024-02-21 11:33:41 -0500

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,3 @@
+let numeros =[21,22,55];
+numeros.push(144,1000);
+alert(numeros);
---
+++ ejercicio9.js
@@ -0,0 +1,28 @@
+let alumnos = [];
+let edadMayor = 0;
+
+while (true) {
+  let nombre = prompt('Introduce el nombre del alumno ingresa un (*) para terminar)');
+  if (nombre === '*') {
+    break;
+  }
+  
+  let edad = parseInt(prompt(`Introduce la edad de ${nombre}`));
+  alumnos.push({ nombre, edad });
+
+  if (edad >= 18) {
+    console.log(`${nombre} es mayor de edad`);
+  }
+
+  if (edad > edadMayor) {
+    edadMayor = edad;
+  }
+}
+
+console.log(`Alumno con edad máxima ${edadMayor}`);
+
+for (let i = 0; i < alumnos.length; i++) {
+  if (alumnos[i].edad === edadMayor) {
+    console.log(alumnos[i].nombre);
+  }
+}
