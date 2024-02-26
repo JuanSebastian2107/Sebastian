a9d73fe9400e0394bf29648f52f17f0dc8427370
Sebastian <juasemh@gmail.com>
2024-02-26 10:50:00 -0500

    practica

--- Practica.js/ejercicio1.js
+++ Practica.js/ejercicio1.js
@@ -1,14 +1,9 @@
-let num = parseInt(prompt('ingresa un numero'));
+function fizzbuzz(numero) {
+let multiplo3 = numero % 3 === 0 ? "fizz" : "";
+let multiplo5 = numero % 5 === 0 ? "buzz" : "";
+  
+return (multiplo3 + multiplo5) || numero.toString();
+}
+let numero = prompt('ingrese un numero');
+ console.log(fizzbuzz(numero));
 
-function cons(num){
-if (num  % 3 === 0 && num % 5 === 0){
-return 'FizzBuzz';
-} else if (num % 3 == 0) {
- return 'Fizz';
-} else if (num % 5 == 0) {
- return 'Buzz';
-} else {
- return num;
-}
-}
-console.log(cons(num))
